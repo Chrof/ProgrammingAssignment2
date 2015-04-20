f4a02d5d7848afb3899a0c0258b8029dd71ec015
Chrof <christian@chrof.de>
2015-04-20 15:22:23 +0200

    Testmatrix

--- cachematrix.R
+++ cachematrix.R
@@ -8,8 +8,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function test
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        solve(x)
 }
+
+
+# debugcode
+#dummy matrix for testing 
+test<-matrix(c(1,2,1,2), nrow=2,ncol=2, byrow=TRUE)
